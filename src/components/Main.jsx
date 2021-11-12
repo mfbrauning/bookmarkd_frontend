e013fed35c4ac1c92484e8bc95c290d4be39850d
faith musa <faithy@faiths-MacBook-Air.local>
2021-11-12 15:38:48 -0600

    update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,43 +1,73 @@
-import Index from "../pages/Index"
-import Show from "../pages/Show"
-import { Route, Routes } from "react-router-dom"
-import { useState, useEffect } from "react"
+import Index from "../pages/Index";
+import Show from "../pages/Show";
+import { Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
 
+function Main(props) {
+  // state
+  const [bookmarks, setBookmarks] = useState(null);
 
-function Main(props){
-    // state
-    const [bookmarks, setBookmarks] = useState(null)
+  //api url
+  const URL = "https://mfb-bookmarkd-backend.herokuapp.com/bookmarks/";
 
-    //api url
-    const URL = "https://mfb-bookmarkd-backend.herokuapp.com/bookmarks/"
+  const getBookmarks = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    setBookmarks(data);
+  };
 
-    const getBookmarks = async () => {
-        const response = await fetch(URL);
-        const data = await response.json();
-        setBookmarks(data)
-    }
+  const createBookmarks = async (bookmark) => {
+    await fetch(URL, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bookmark),
+    });
+    getBookmarks();
+  };
 
-    const createBookmarks = async (bookmark) => {
-        await fetch(URL, {
-            method: "post",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(bookmark)
-        })
-        getBookmarks()
-    }
+  const updateBookmarks = async (bookmark, id) => {
+    await fetch(URL + id, {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bookmark),
+    });
+    getBookmarks();
+  };
+  const deleteBookmarks = async (id) => {
+    await fetch(URL + id, {
+      method: "delete",
+    });
+    getBookmarks();
+  };
 
-    useEffect(() => getBookmarks(), [])
+  useEffect(() => getBookmarks(), []);
 
-
-
-    return <div>
-        <Routes>
-            <Route path="/" element={<Index bookmarks={bookmarks} createBookmarks={createBookmarks}/>}/>
-            <Route path="/bookmarks/:id" element={<Show/>}/>
-        </Routes>
+  return (
+    <div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Index bookmarks={bookmarks} createBookmarks={createBookmarks} />
+          }
+        />
+        <Route
+          path="/bookmarks/:id"
+          element={
+            <Show
+              bookmarks={bookmarks}
+              updateBookmarks={updateBookmarks}
+              deleteBookmarks={deleteBookmarks}
+            />
+          }
+        />
+      </Routes>
     </div>
+  );
 }
 
-export default Main
+export default Main;
--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,5 +1,62 @@
-function Show(props){
-    return <div>Show</div>
+import { useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+function Show(props) {
+  const navigate = useNavigate();
+  const params = useParams();
+  const id = params.id;
+  const bookmarks = props.bookmarks;
+  const [editForm, setEditForm] = useState({});
+  useEffect(() => {
+    if (props.bookmarks) {
+      const bookmark = bookmarks.find((b) => b._id === id);
+      setEditForm(bookmark);
+    }
+  }, [props.bookmarks]);
+
+  if (props.bookmarks) {
+    const bookmark = bookmarks.find((b) => b._id === id);
+    const handleChange = (event) => {
+      const newState = { ...editForm };
+      newState[event.target.name] = event.target.value;
+      setEditForm(newState);
+    };
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      props.updateBookmarks(editForm);
+      navigate("/");
+    };
+    const form = (
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">
+          <input
+            type="text"
+            value={editForm.name}
+            name="name"
+            placeholder="name"
+            onChange={handleChange}
+          />
+        </label>
+        <input
+          type="text"
+          value={editForm.url}
+          name="url"
+          placeholder="Bookmark URL"
+          onChange={handleChange}
+        />
+        <input type="submit" value="Edit Bookmark" />
+      </form>
+    );
+    return (
+      <div className="bookmark">
+        <h1>{bookmark.name}</h1>
+
+        {form}
+      </div>
+    );
+  } else {
+    <h1>okay</h1>;
+  }
 }
 
-export default Show
+export default Show;
