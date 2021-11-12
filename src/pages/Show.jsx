5e07a3b8f02c9ca30ed9440634710d96f0706d38
Anthony Gonzales <anthonygonzales79072@gmail.com>
2021-11-12 15:01:02 -0600

    delete Route

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,10 +1,46 @@
-import {useParams, useNavigate } from "react-router-dom"
-import 
+import { useParams, useNavigate} from "react-router-dom";
+import {useState, useEffect} from "react"
 
 function Show(props){
-  
+    const navigate = useNavigate()
+    const params = useParams()
+    const id = params.id
+    const bookmark = props.bookmarks
+    // Add State
+    const[editForm, setEditForm] = useState({})
+    // useEffect to avoid show route page refresh error
+    useEffect(() => {
+        if(props.bookmarks){
+            // Grab the id
+            const bookmarks = bookmark.find((b) => b._id === id)
+            setEditForm(bookmarks)
+        }
+    }, [props.bookmark])
 
-    return <div>Show</div>
+    if (props.bookmark){
+        //Grab the id 
+        const bookmarks = bookmark.find((b) => b._id === id)
+
+
+
+
+        // Delete function 
+        const removeBookmark = () => {
+            props.deleteBookmark(bookmarks._id)
+            navigate("/")
+         }
+         return (
+             <div className="bookmarks">
+                 <button id="delete" onClick={removeBookmark}>
+                Delete Bookmark
+                </button>
+
+             </div>
+         )
+    }else{
+        return<h1>No Bookmark</h1>
+    }
+     
 }
 
 export default Show
