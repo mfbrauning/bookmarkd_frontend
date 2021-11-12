42a7c7189189445ae3959f34fd2a7ce1e5ed5a3f
Maria Francisca Brauning <franbrauning@gmail.com>
2021-11-12 15:31:37 -0500

    added index and create new bookmark

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,40 @@
 import Index from "../pages/Index"
 import Show from "../pages/Show"
 import { Route, Routes } from "react-router-dom"
+import { useState, useEffect } from "react"
 
 
 function Main(props){
+    // state
+    const [bookmarks, setBookmarks] = useState(null)
+
+    //api url
+    const URL = "https://mfb-bookmarkd-backend.herokuapp.com/bookmarks/"
+
+    const getBookmarks = async () => {
+        const response = await fetch(URL);
+        const data = await response.json();
+        setBookmarks(data)
+    }
+
+    const createBookmarks = async (bookmark) => {
+        await fetch(URL, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(bookmark)
+        })
+        getBookmarks()
+    }
+
+    useEffect(() => getBookmarks(), [])
+
+
+
     return <div>
         <Routes>
-            <Route path="/" element={<Index/>}/>
+            <Route path="/" element={<Index bookmarks={bookmarks} createBookmarks={createBookmarks}/>}/>
             <Route path="/bookmarks/:id" element={<Show/>}/>
         </Routes>
     </div>
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,5 +1,69 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+
 function Index(props){
-    return <div>Index</div>
+    const [newForm, setNewForm] = useState({
+        name: "",
+        url: ""
+    })
+    
+    const handleChange = (event) => {
+        const newState = {...newForm}
+        newState[event.target.name] = event.target.value 
+        setNewForm(newState)
+    }
+
+    const handleSumbit = (event) => {
+        event.preventDefault()
+        props.createBookmarks(newForm)
+        setNewForm({
+            name: "",
+            url: ""
+        })
+    }
+
+    const form = <form onSubmit={handleSumbit}>
+        <input
+        type="text"
+        value={newForm.name}
+        name="name"
+        placeholder="name"
+        onChange={handleChange}/>
+         <input
+        type="text"
+        value={newForm.url}
+        name="url"
+        placeholder="Bookmark URL"
+        onChange={handleChange}/>
+        <input type="submit" value="Add New Bookmark"/>
+    </form>
+
+
+    if (props.bookmarks) {
+        return (
+          <section>
+            {form}
+            {props.bookmarks.map((bookmark) => {
+              return (
+                <div key={bookmark._id} className="bookmark">
+
+                <a href={bookmark.url} alt={bookmark.name}>{bookmark.name}</a>
+                <Link to={`/bookmarks/${bookmark._id}`}>
+                  <h1>Edit Bookmark</h1>
+                </Link>
+              </div>
+              );
+            })}
+          </section>
+        );
+      } else {
+        return (
+          <section>
+              {form}
+            <h1>Loading...</h1>
+          </section>
+        );
+      }
 }
 
 export default Index
